8b415cbe8c27b67b926762ab3bb80df1d493a583
Mourad <mod7ex@mail.com>
2026-01-19 22:06:02 +0100

    save

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,9 +1,11 @@
-import pandas as pd
+# import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
-# GDP_df
-def real_gdp_df():
-    GDP_df = pd.read_csv('data/A191RL1Q225SBEA.csv')
-    GDP_df['DATE'] = pd.to_datetime(GDP_df['DATE'])
-    GDP_df.set_index('DATE', inplace=True)
-    GDP_df.columns = ['GDP']
-    return GDP_df
+# # GDP_df
+# def real_gdp_df():
+#     GDP_df = pd.read_csv('data/A191RL1Q225SBEA.csv')
+#     GDP_df['DATE'] = pd.to_datetime(GDP_df['DATE'])
+#     GDP_df.set_index('DATE', inplace=True)
+#     GDP_df.columns = ['GDP']
+#     return GDP_df
